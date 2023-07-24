9354ebe5f56f883991eb9a91e9d580fd7f2b590f
Fauzanyan <ccdplayerx@gmail.com>
2023-07-25 06:42:21 +0700

    api

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 export default axios.create({
-  baseURL: import.meta.env.VITE_APP_BACKEND_URL,
+  // baseURL: import.meta.env.VITE_APP_BACKEND_URL,
+  baseURL: 'http://54.80.111.38:8005',
+
   timeout: 15000,
   headers: {
     Accept: 'application/json',
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer ' + 'test_1'
+    'Content-Type': 'application/json'
+    // Authorization: "Bearer "+ token,
   }
 })
